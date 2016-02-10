d307414953cf3babe0f1e0ae5169e9c9d98162cb
Hugo A Guerra <guerrahugoa@gmail.com>
2016-02-10 13:14:59 -0800

    added sql functionality to createTable method

--- class-08-crud-a-resource/hugo-stephen/scripts/article.js
+++ class-08-crud-a-resource/hugo-stephen/scripts/article.js
@@ -22,7 +22,15 @@
   // TODO: Set up a DB table for articles.
   Article.createTable = function(callback) {
     webDB.execute(
-      '...',
+      'CREATE TABLE IF NOT EXISTS articles(' +
+      'id INTEGER PRIMARY KEY, ' +
+      'title VARCHAR(255) NOT NULL, ' +
+      'category VARCHAR(75) NOT NULL, ' +
+      'author VARCHAR(145) NOT NULL, ' +
+      'authorURL VARCHAR(300) NOT NULL, ' +
+      'publishedOn DATE NOT NULL, ' +
+      'body VARCHAR(10000) NOT NULL'
+    ');',
       function(result) {
         console.log('Successfully set up the articles table.', result);
         if (callback) callback();
