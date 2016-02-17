9a3ef55c5eabc2f3f48b242e38b67a510b9c32e6
Hugo A Guerra <guerrahugoa@gmail.com>
2016-02-16 20:46:59 -0800

    changed needed token.

--- class-12-REST-and-APIs/hugo-pedja/scripts/repoView.js
+++ class-12-REST-and-APIs/hugo-pedja/scripts/repoView.js
@@ -1,7 +1,7 @@
 (function(module) {
   var repoView = {};
 
-  // DONE: Private methods declared here live only within the scope of the wrapping IIFE.
+  // DONE: Private methods declared here live only wthn the scope of the wrapping IIFE.
   var ui = function() {
     var $about = $('#about'); // Best practice: Cache the DOM query if it's used more than once.
 
